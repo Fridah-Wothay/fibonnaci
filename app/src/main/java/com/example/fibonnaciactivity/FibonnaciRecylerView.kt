2e2318ab88e61e825d1764a5e6847213da0893e4
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-06-19 14:26:41 +0300

    add file

--- app/src/main/java/com/example/fibonnaciactivity/FibonnaciRecylerView.kt
+++ app/src/main/java/com/example/fibonnaciactivity/FibonnaciRecylerView.kt
@@ -4,25 +4,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
 
-class fibonnacirecylerview(var items:List<Int>): RecyclerView.Adapter<displaynumbers>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): displaynumbers {
+class FibonnaciRecyclerView(var items:List<Int>): RecyclerView.Adapter<Displaynumbers>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Displaynumbers {
        var itemView = LayoutInflater.from(parent.context)
            .inflate(R.layout.fibonnaci_list_items,parent,false)
-        return displaynumbers(itemView)
+        return Displaynumbers(itemView)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    override fun onBindViewHolder(holder: displaynumbers, position: Int) {
+    override fun onBindViewHolder(holder: Displaynumbers, position: Int) {
        holder.tvNumbers.text = items[position].toString()
     }
 
 }
-class displaynumbers(itemView: View): RecyclerView.ViewHolder(itemView){
+class Displaynumbers(itemView: View): RecyclerView.ViewHolder(itemView){
     var tvNumbers = itemView.findViewById<TextView>(R.id.tvNumbers)
 }
--- app/src/main/java/com/example/fibonnaciactivity/MainActivity.kt
+++ app/src/main/java/com/example/fibonnaciactivity/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.fibonnaciactivity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fibonnaciactivity.databinding.ActivityMainBinding
-import com.example.fibonnaciactivity.databinding.FibonnaciListItemsBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var  binding: ActivityMainBinding
@@ -19,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val items= displayfibonnaci(100)
         binding.rNumber.layoutManager = LinearLayoutManager(this)
 
-        val numberAdapter = fibonnacirecylerview(items)
+        val numberAdapter = FibonnaciRecyclerView(items)
         binding.rNumber.adapter = numberAdapter
 
 
